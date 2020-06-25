2c73ba3517a86472528bd4416cb9bd19665da0b9
Jinny Choi <jinchoi99@fb.com>
2020-06-25 05:36:38 -0700

    Create list of Movies

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -1,7 +1,11 @@
 package com.example.flixster.models;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Movie {
 
@@ -14,6 +18,15 @@ public class Movie {
          posterPath = jsonObject.getString("poster_path");
          title = jsonObject.getString("title");
          overView = jsonObject.getString("overview");
+    }
 
+    //iterate through movieJsonArray and construct Movie object for each movie
+    //this is a static method -> call by Movie.fromJsonArray(), not by instantiating Movie object Ob1 and then calling Ob1.fromJsonArray()
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for(int i=0; i<movieJsonArray.length(); i++){
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
     }
 }
