dff0479414415f0b5f53fdd978d58b0accada11d
Jinny Choi <jinchoi99@fb.com>
2020-06-26 04:12:25 -0700

    Integrate Parceler

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,16 +3,21 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel // annotation indicates class is Parcelable
 public class Movie {
-
+    // fields must be public for parceler
     String backdropPath;
     String posterPath;
     String title;
     String overView;
+
+    // no-arg, empty constructor required for Parceler
+    public Movie() {}
 
     //Construct Movie by parsing through data in jsonObject
     public Movie(JSONObject jsonObject) throws JSONException {
