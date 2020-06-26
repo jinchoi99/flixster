e98b12cbb945c293e9705a1747205f1afe0898bb
Jinny Choi <jinchoi99@fb.com>
2020-06-26 12:54:56 -0700

    Add secrets.xml

--- app/src/main/java/com/example/flixster/MainActivity.java
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -24,6 +24,8 @@ import okhttp3.Headers;
 public class MainActivity extends AppCompatActivity {
 
     //Sample API request
+    //api = application programming interface => interaction btw my flixster app and themoviedb app
+    //api get request for now_playing method -> request from my flixster app to themoviedb app through the Movie API
     public static final String NOW_PLAYING_URL =" https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
     public static final String TAG = "MainActivity";
 
