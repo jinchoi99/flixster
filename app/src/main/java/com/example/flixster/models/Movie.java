9840ce64906713491f8a0a24762119888dd0f6bb
Jinny Choi <jinchoi99@fb.com>
2020-06-26 00:03:52 -0700

    Add backdropPath field in Movie class

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -9,12 +9,14 @@ import java.util.List;
 
 public class Movie {
 
+    String backdropPath;
     String posterPath;
     String title;
     String overView;
 
     //Construct Movie by parsing through data in jsonObject
     public Movie(JSONObject jsonObject) throws JSONException {
+        backdropPath = jsonObject.getString("backdrop_path");
          posterPath = jsonObject.getString("poster_path");
          title = jsonObject.getString("title");
          overView = jsonObject.getString("overview");
@@ -32,6 +34,10 @@ public class Movie {
 
     public String getPosterPath() {
         return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
     }
 
     public String getTitle() {
