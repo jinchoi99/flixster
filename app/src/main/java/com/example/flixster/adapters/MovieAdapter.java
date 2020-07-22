15fcd7e7294a018bb869f5ec814c49728cd88b3f
Jinny Choi <jinchoi99@fb.com>
2020-07-22 12:26:16 -0700

    explain interface

--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -72,7 +72,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
-            // add this as the itemView's OnClickListener
+            // add this as the itemView's OnClickListener (implements OnClickListener interface)
             itemView.setOnClickListener(this);
         }
 
